1663804960fad39cf3bd5c24db37dd287a5bdf5f
Darshana Edirisinghe <mihiranlumiaa@outlook.com>
2024-05-20 21:51:16 +0530

    add health check endpoint

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+app.get('/health', (req, res) => {
+  res.header({ "System-Health": true })
+  res.sendStatus(204)
+})
+
 
 app.listen(3000, () => {
   console.log("listening on 3000");
